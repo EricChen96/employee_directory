bd81e2eac6c18b9bfd4b83f3a36fbec9a2e660ee
Eric Chen <ericchen201896@gmail.com>
2021-02-14 13:24:56 -0800

    Created the header bar for the page with css

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -3,7 +3,7 @@ import "./styles/HeaderBar.css"
 function HeaderBar(props) {
     return (
         <div className = "header">
-            <h1>Employee Directory</h1>
+            <h1 className = "websiteTitle">Employee Directory</h1>
             <div>Click on carrots to filter by heading or use the search box to narrow your results.</div>
         </div>
     )
